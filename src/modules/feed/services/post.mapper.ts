f884f05e457fd8282ebc14ce974fe4a59e1f738c
PirateSeal <tcousin.pseal@gmail.com>
2021-01-28 17:10:41 +0100

    feed regex

--- src/modules/feed/services/post.mapper.ts
+++ src/modules/feed/services/post.mapper.ts
@@ -1,54 +1,61 @@
-import { MessageAudioElement, MessageElement, MessageImageElement, MessageTextElement, MessageVideoElement, MessageYoutubeElement, Post, PostData, PostMessage } from '../post.model';
+import {
+  MessageAudioElement,
+  MessageElement,
+  MessageImageElement,
+  MessageTextElement,
+  MessageVideoElement,
+  MessageYoutubeElement,
+  Post,
+  PostData,
+  PostMessage,
+} from '../post.model';
 
 export class PostMapper {
   map(data: PostData): Post {
     return {
       ...data,
-      message: this.parseMessage(`${data.message} ${data.attachementUrl ? data.attachementUrl : ''}`)
-    }
+      message: this.parseMessage(
+        `${data.message} ${data.attachementUrl ? data.attachementUrl : ''}`
+      ),
+    };
   }
 
   private parseMessage(message: string): PostMessage {
-    // TODO rajouter png jpg et gif
-    const pictureRegex = /http[s]?:\/\/.+\.(jpeg|jpg)/gmi;
+    const pictureRegex = /http[s]?:\/\/.+\.(jpeg|jpg|gif|png)/gim;
 
-     // TODO mp4,wmv,flv,avi,wav
-    const videoRegex = / /gmi;
+    const videoRegex = /http[s]?:\/\/.+\.(mp4|wmv|flv|avi|wav)/gim;
 
-     // TODO mp3,ogg,wav
-    const audioRegex = / /gmi;
+    const audioRegex = /http[s]?:\/\/.+\.(mp3|ogg|wav|m4a)/gim;
 
-    const youtubeRegex = /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
+    const youtubeRegex = /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gim;
     const attachements: MessageElement[] = [];
 
     const pictureMatche = pictureRegex.exec(message);
     if (pictureMatche) {
-     // TODO ajouter un attachement de type image dans attachements
+      // TODO ajouter un attachement de type image dans attachements
     }
 
-    const videoMatche = videoRegex.exec(message)
+    const videoMatche = videoRegex.exec(message);
     if (videoMatche) {
-     // TODO ajouter un attachement de type video dans attachements
-
+      // TODO ajouter un attachement de type video dans attachements
     }
 
-    const audioMatche = audioRegex.exec(message)
+    const audioMatche = audioRegex.exec(message);
     if (audioMatche) {
-     // TODO ajouter un attachement de type audio dans attachements
-
+      // TODO ajouter un attachement de type audio dans attachements
     }
 
-    const youtubeMatche = youtubeRegex.exec(message)
+    const youtubeMatche = youtubeRegex.exec(message);
     if (youtubeMatche) {
-     // TODO ajouter un attachement de type youtube dans attachements
+      // TODO ajouter un attachement de type youtube dans attachements
     }
 
     return {
       text: {
         type: 'text',
-        content: message
+        content: message,
       } as MessageTextElement,
-      attachements
+      attachements,
     };
   }
 }
